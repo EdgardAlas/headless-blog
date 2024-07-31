6265dca0811c76ff8a4479ba057c2a9fa259630a
Edgard Alas <edgardalasu@gmail.com>
2024-07-31 17:43:56 -0600

    refactor: update CRUD map with validation from validations-map

--- src/crud/crud-map.tsx
+++ src/crud/crud-map.tsx
@@ -1,31 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { UsersTableColumns } from '@/crud/users/_containers/users-columns';
-import * as usersActions from '@/crud/users/actions';
 import { getUsersPaginated } from '@/crud/users/dao';
-import * as usersValidations from '@/crud/users/validations';
+import { validatiodCrudMap } from '@/crud/validations-map';
 import { CrudMap } from '@/types/crud';
 
 export const crudMap: CrudMap = {
 	users: {
 		roles: ['admin'],
 		columns: UsersTableColumns,
-		crud: {
-			create: {
-				title: 'Create User',
-				validation: usersValidations.createUserValidation,
-			},
-			update: {
-				title: 'Update User',
-				validation: usersValidations.updateUserValidation,
-			},
-			delete: {
-				title: 'Delete User',
-			},
-			deleteAction: usersActions.deleteUserAction,
-			createAction: usersActions.createUserAction,
-			updateAction: usersActions.updateUserAction,
-			getAction: usersActions.getUserByIdAction,
-		},
+		crud: validatiodCrudMap.users,
 		getData: getUsersPaginated,
 		fieldConfig: {
 			password: {
