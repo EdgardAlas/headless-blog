e3032b3b5dfc1bf5f1085167050861c391f8a1ae
Edgard Alas <edgardalasu@gmail.com>
2024-08-02 16:22:04 -0600

    refactor: Update NotFound component with styled UI and button

--- src/app/(dashboard)/not-found.tsx
+++ src/app/(dashboard)/not-found.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Button } from '@components/ui/button';
+import Link from 'next/link';
 
 const NotFound = () => {
-	return <div>NotFound</div>;
+	return (
+		<div className='flex w-full flex-col items-center justify-center gap-4 px-4'>
+			<div className='space-y-2 text-center'>
+				<h1 className='text-4xl font-bold tracking-tighter'>
+					404 - Page Not Found
+				</h1>
+				<p className='text-gray-500 dark:text-gray-400'>
+					The page you are looking for does not exist or has been moved.
+				</p>
+			</div>
+			<Button asChild>
+				<Link href='/'>Go back to home</Link>
+			</Button>
+		</div>
+	);
 };
 
 export default NotFound;
