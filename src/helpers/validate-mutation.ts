579e3af20134b5b2f757e3cd019d8342e6e9de66
Edgard Alas <edgardalasu@gmail.com>
2024-08-05 13:15:27 -0600

    feat: Update validateMutation to allow 'super-admin' role

--- src/helpers/validate-mutation.ts
+++ src/helpers/validate-mutation.ts
@@ -2,6 +2,10 @@ import { Role } from '@/constants/roles';
 import { CustomError } from '@/helpers/custom-error';
 
 export const validateMutation = (role: Role, permittedRoles: Role[]) => {
+	if (role === 'super-admin') {
+		return;
+	}
+
 	if (!permittedRoles.includes(role)) {
 		throw new CustomError('You do not have permission');
 	}
